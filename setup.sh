0033aba17dfe3a3c1a019510caf7f7738f62db63
ret2basic <ret2basic.com>
2021-07-18 14:50:40 -0400

    Pwnie Island rocks

--- setup.sh
+++ setup.sh
@@ -7,9 +7,28 @@ git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
+# Oh My Tmux is tmux enhancement
+
 #--------Updog--------#
 
 sudo pip3 install updog
+
+# updog is an alternative to Python HTTP server
+
+# Host files on port 80:
+# $ updog -p 80
+
+#--------pwncat--------#
+
+sudo pip3 install git+https://github.com/calebstewart/pwncat.git
+
+# pwncat gives you better reverse shell
+
+# Start a listener:
+# $ pwncat :443
+
+# SSH connection:
+# $ pwncat <username>@<remote_ip>
 
 #--------emp3r0r--------#
 
