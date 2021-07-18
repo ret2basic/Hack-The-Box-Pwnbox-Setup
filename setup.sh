b4cc4938e8dbbf5ee3b18a02cef9b32da710ca62
ret2basic <ret2basic.com>
2021-07-18 14:24:43 -0400

    Pwnie Island rocks

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#--------Oh My Tmux--------#
+
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+#--------Updog--------#
+
+sudo pip3 install updog
+
+#--------emp3r0r--------#
+
+git clone git@github.com:jm33-m0/emp3r0r.git
